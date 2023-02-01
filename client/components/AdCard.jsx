8576038814f7f6c718fb1ed2e8fe2cead26ae43a
Vincent Jacquemin <vincent@impulsenow.be>
2023-02-01 21:47:51 +0100

    react connected to db

---
+++ client/components/AdCard.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const AdCard = ({ info, openModal }) => {
+  const {
+    _id,
+    title,
+    description,
+    width,
+    height,
+    BG_type,
+    BG_color,
+    BG_image,
+    HL_font_family,
+    headline,
+    SHL_font_family,
+    sub_headline,
+    CTA,
+    CTA_font_family,
+    CTA_BG_color,
+    CTA_FONT_color,
+    CTA_link,
+    author_id,
+  } = info;
+  console.log(info);
+  return (
+    <article
+      className='card charCard'
+      style={{
+        backgroundColor: info['BG-color'],
+      }}
+    >
+      <div className='charHeadContainer'>
+        <h3 className='charName'>{title}</h3>
+      </div>
+      <ul className='charDetailsList'>
+        <li className='charDetail'>Description: {description}</li>
+      </ul>
+    </article>
+  );
+};
+
+export default AdCard;
--- client/components/Overview.jsx
+++ client/components/Overview.jsx
@@ -1,10 +1,66 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-function Overview(props) {
-  return (
-    <div>
-      <div class='overviewItem'></div>
-    </div>
-  );
+import AdCard from './AdCard';
+
+class Overview extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fetchedChars: false,
+      ads: [],
+      modalState: {
+        open: false,
+      },
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api/')
+      .then((res) => res.json())
+      .then((ads) => {
+        if (!Array.isArray(ads)) ads = [];
+        return this.setState({
+          ads,
+          fetchedChars: true,
+        });
+      })
+      .catch((err) =>
+        console.log('Overview.componentDidMount: get ads: ERROR: ', err)
+      );
+  }
+
+  render() {
+    if (!this.state.fetchedChars)
+      return (
+        <div>
+          <h1>Loading data, please wait...</h1>
+        </div>
+      );
+
+    const { ads } = this.state;
+
+    if (!ads) return null;
+
+    if (!ads.length) return <div>Sorry, no characters found</div>;
+
+    const adElems = ads.map((char, i) => {
+      return <AdCard key={i} info={char} openModal={this.openModal} />;
+    });
+
+    return (
+      <section className='mainSection'>
+        <header className='pageHeader'>
+          <h2>Ads</h2>
+          <Link to={'/create'}>
+            <button type='button' className='btnSecondary'>
+              Create a new ad!
+            </button>
+          </Link>
+        </header>
+        <div className='adContainer'>{adElems}</div>
+      </section>
+    );
+  }
 }
 export default Overview;
